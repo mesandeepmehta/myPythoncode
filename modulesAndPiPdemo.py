b74b533096faa71f5eea19d7e09900bd636c93f0
mesandeepmehta <119277325+mesandeepmehta@users.noreply.github.com>
2023-03-11 09:18:27 +0530

    added example for object and class

--- modulesAndPiPdemo.py
+++ modulesAndPiPdemo.py
@@ -1,2 +1,7 @@
 import usefulTools  #imported my useful tools file
+from object import Cars
 print(usefulTools.rollDice(6))   #this function is defined on usefulTools
+
+car1 = Cars("Toyota", "Fortuner", "red", 2500000)
+car2 = Cars("Mercedes", "S-Class", "JetBlack", 12500000)
+print (car2.model)
---
+++ object.py
@@ -0,0 +1,6 @@
+class Cars:
+    def __init__(self, name, model, color, price):
+        self.name=name
+        self.model=model
+        self.color=color
+        self.price=price
